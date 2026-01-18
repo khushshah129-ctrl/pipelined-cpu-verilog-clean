5f57b296366b8a7c4b4d94f8d7d7ed0f2d5698c8
Khush  shah <khushshah129@gmail.com>
2026-01-18 14:16:59 +0530

    Clean up comments in DeFrame.v
    Removed author comments and metadata from the DeFrame module.

--- UART/Code/Full-Body/DeFrame.v
+++ UART/Code/Full-Body/DeFrame.v
@@ -1,12 +1,4 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: DeFrame.v
-//  TYPE: module.
-//  DATE: 31/8/2022
-//  KEYWORDS: Frame, Data.
-//  PURPOSE: An RTL modelling for separating the frame
-//  into Data bits, Parity bit, Start and Stop bits.
+
 
 module DeFrame(
     input wire          reset_n,        //  Active low reset.
