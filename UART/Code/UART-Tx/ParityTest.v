03faf5c09089f40b9cab45a788a02afaaa356fc1
Khush  shah <khushshah129@gmail.com>
2026-01-18 14:21:57 +0530

    Clean up comments in ParityTest.v
    Removed author information and metadata comments.

--- UART/Code/UART-Tx/ParityTest.v
+++ UART/Code/UART-Tx/ParityTest.v
@@ -1,9 +1,4 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: ParityTest.v
-//  TYPE: Test fixture "Test bench".
-//  DATE: 30/8/2022
+
 //  KEYWORDS: Parity.
 
 `timescale 1ns/1ps
