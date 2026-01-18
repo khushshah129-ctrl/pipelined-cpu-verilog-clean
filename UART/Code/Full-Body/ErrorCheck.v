e53602f72400aa1fd088dffd4ee5451db21d1b35
Khush  shah <khushshah129@gmail.com>
2026-01-18 14:18:31 +0530

    Clean up comments in ErrorCheck.v
    Removed author information and comments from the ErrorCheck.v file.

--- UART/Code/Full-Body/ErrorCheck.v
+++ UART/Code/Full-Body/ErrorCheck.v
@@ -1,12 +1,4 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: ErrorCheck.v
-//  TYPE: module.
-//  DATE: 31/8/2022
-//  KEYWORDS: Parity, Error.
-//  PURPOSE: An RTL modelling for checking if the data are transmitted correctly,
-//  checks the whole package including the start and stop bits.
+
 
 module ErrorCheck(
     input wire         reset_n,       //  Active low reset.
