5187d6fd692b9772b50abb1b925933261bf12647
Khush  shah <khushshah129@gmail.com>
2026-01-19 21:30:53 +0530

    Clean up comments in CheckTest.v
    Removed author information and metadata comments.

--- UART/Code/UART-Rx/CheckTest.v
+++ UART/Code/UART-Rx/CheckTest.v
@@ -1,10 +1,4 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: CheckTest.v
-//  TYPE: Test fixture "Test bench".
-//  DATE: 31/8/2022
-//  KEYWORDS: Parity, Error.
+
 
 `timescale 1ns/1ps
 module CheckTest;
