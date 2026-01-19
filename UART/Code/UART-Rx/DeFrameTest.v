cdcdf03cceec98bc59684d6b892ea1ef008de3fd
Khush  shah <khushshah129@gmail.com>
2026-01-19 21:31:37 +0530

    Clean up comments in DeFrameTest.v
    Removed author information and metadata comments.

--- UART/Code/UART-Rx/DeFrameTest.v
+++ UART/Code/UART-Rx/DeFrameTest.v
@@ -1,10 +1,4 @@
-//  AUTHOR: Mohamed Maged Elkholy.
-//  INFO.: Undergraduate ECE student, Alexandria university, Egypt.
-//  AUTHOR'S EMAIL: majiidd17@icloud.com
-//  FILE NAME: DeFrameTest.v
-//  TYPE: Test fixture "Test bench".
-//  DATE: 31/8/2022
-//  KEYWORDS: Frame, Data.
+
 
 `timescale 1ns/1ps
 module DeFrameTest;
